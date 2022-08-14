d8e4dea259e0b7f3e6a3fe13b1448a897828c726
Hidenori Matsuki <dev@mazgi.com>
2022-08-14 20:46:34 +0900

    import basic resouces from my public example.

---
+++ provisioning/azure-appgw-with-vm/vnet.tf
@@ -0,0 +1,31 @@
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.project_unique_id}-main"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  tags                = {}
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "appgw" {
+  name                 = "${var.project_unique_id}-main-appgw"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.0.0/24"]
+}
+
+resource "azurerm_subnet_network_security_group_association" "appgw" {
+  subnet_id                 = azurerm_subnet.appgw.id
+  network_security_group_id = azurerm_network_security_group.main.id
+}
+
+resource "azurerm_subnet" "vms" {
+  name                 = "${var.project_unique_id}-vms"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.3.0/24"]
+}
+
+resource "azurerm_subnet_network_security_group_association" "vms" {
+  subnet_id                 = azurerm_subnet.vms.id
+  network_security_group_id = azurerm_network_security_group.main.id
+}
